61d23bfea499b374dba55f4510f98d0bf3ef08d7
Alex Ross <alexander.w.rosss@gmail.com>
2020-04-07 10:33:37 -0400

    added screens files

---
+++ screens/MealDetailScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const MealDetailScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The meal details screen</Text>
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
+export default MealDetailScreen;
